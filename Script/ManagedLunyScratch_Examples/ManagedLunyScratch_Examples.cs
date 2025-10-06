51c07b2e6278784a2cdbe9fcedfce3f3e1877c57
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-10-06 15:15:37 +0200

    managed project files

---
+++ Script/ManagedLunyScratch_Examples/ManagedLunyScratch_Examples.cs
@@ -0,0 +1,16 @@
+using UnrealSharp.Engine.Core.Modules;
+
+namespace ManagedLunyScratch_Examples;
+
+public class FManagedLunyScratch_Examples : IModuleInterface
+{
+    public void StartupModule()
+    {
+
+    }
+
+    public void ShutdownModule()
+    {
+
+    }
+}
