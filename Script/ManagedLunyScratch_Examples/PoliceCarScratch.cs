71d1afba90b428bed83eff1560e0c50d24c06f08
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-10-19 16:16:51 +0200

    add police car with blinking lights

--- Script/ManagedLunyScratch_Examples/PoliceCarScratch.cs
+++ Script/ManagedLunyScratch_Examples/PoliceCarScratch.cs
@@ -1,73 +1,30 @@
 ﻿using LunyScratch;
 using UnrealSharp.Attributes;
-using UnrealSharp.CoreUObject;
-using UnrealSharp.Engine;
 using static LunyScratch.Blocks;
 
 namespace ManagedScratchTest10
 {
 	[UClass]
-	public /*abstract*/ class AScratchActor2 : AActor
+	public class APoliceCarScratch : AScratchActor
 	{
-		protected override void BeginPlay()
+		protected override void OnComponentReady()
 		{
-			base.BeginPlay();
-			OnReady();
-		}
-
-		public override void Tick(Single deltaTime)
-		{
-			base.Tick(deltaTime);
-			OnUpdate(deltaTime);
-		}
-
-		protected virtual void OnReady() {}
-		protected virtual void OnUpdate(Single deltaTime) {}
-	}
-
-	[UClass]
-	public class APoliceCarScratch : AScratchActor2
-	{
-		private Single _accelerateDelay = 1.2f;
-		private Single _acceleration = 0.312f;
-		private Single _maxSpeed = 2f;
-		private Single _stopX = 9f;
-		private Single _stopVelocitySlowdown = 0.95f;
-		private Single _stopVelocityY = -1f;
-
-		private Single _speed;
-		private FVector _heading;
-		private UPrimitiveComponent? _car;
-
-		protected override void BeginPlay()
-		{
-			base.BeginPlay();
-
 			ActorTickEnabled = true;
-			_car = GetComponentByClass<UPrimitiveComponent>();
-			_heading = ActorForwardVector;
+			//_car = GetComponentByClass<UPrimitiveComponent>();
 
-
-		}
-
-		private void MoveCar()
-		{
-			//PrintString($"MoveCar: {_car}");
-
-			// if (transform.position.x < _stopX)
-			// {
-			// 	_speed = 0f;
-			// 	var velocity = _rigidbody.linearVelocity;
-			// 	velocity *= _stopVelocitySlowdown;
-			// 	velocity.y = -_stopVelocityY;
-			// 	_rigidbody.linearVelocity = velocity;
-			// }
-			// else if (_speed < _maxSpeed)
-			{
-				_speed += _acceleration;
-				var velocity = _car.GetPhysicsLinearVelocity();
-				_car.SetPhysicsLinearVelocity(velocity + _heading * _speed);
-			}
+			// blinking signal lights
+			RepeatForever(
+				Enable("RedLight"),
+				Wait(0.16),
+				Disable("RedLight"),
+				Wait(0.12)
+			);
+			RepeatForever(
+				Disable("BlueLight"),
+				Wait(0.13),
+				Enable("BlueLight"),
+				Wait(0.17)
+			);
 		}
 	}
 }
